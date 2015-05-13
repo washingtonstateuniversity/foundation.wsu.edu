adec37a3c334b73de8421fb484aeb099ebae39d8
jeremyfelt <jeremy.felt@gmail.com>
2015-05-13 15:08:04 -0700

    Add global footer

---
+++ parts/footers.php
@@ -0,0 +1,20 @@
+<section class="row side-right state-silhouette">
+	<div class="column one ">
+		<figure><img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/images/Wash-State-Silhouette-gray1.svg' ); ?>" alt="Wash-State-Silhouette" class="alignnone size-medium">
+		</figure>
+	</div>
+	<div class="column two "></div>
+</section>
+<section class="row single campaign-logo-bar">
+	<div class="column one ">
+		<img class="alignnone" src="https://repo.wsu.edu/spine/develop/marks/wsu-foundation-signature-horizontal-white.svg" alt="Washington State University Foundation" width="409" height="30">
+	</div>
+</section>
+<section class="row single contact-footer">
+	<div class="column one ">
+		<p>Town Centre Building, 3rd Floor • 255 E. Main Street, Suite 301
+			<br> PO Box 641925 • Pullman, WA 99164-1925
+			<br> Phone: 509-335-6686, or 800-GIV-2-WSU (448-2978)
+			<br> Fax: 509-335-4788 • Email: foundation@wsu.edu</p>
+	</div>
+</section>
