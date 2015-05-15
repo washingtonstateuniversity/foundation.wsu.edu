4d6d611d7b21563d37064241c9cee4b7b789f355
jeremyfelt <jeremy.felt@gmail.com>
2015-05-15 15:55:48 -0700

    Introduce campaign progress bars as a CPT and shortcode

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php
 
+include_once( __DIR__ . '/includes/plugin-campaign-progress-shortcode.php' );
+
 add_filter( 'wsu_color_palette_values', 'wsu_foundation_color_palette' );
 function wsu_foundation_color_palette( $palletes ) {
 	$new_palettes = array(
