85ce7dd0bad353a2fb0270a4fe97fbdeaba2f2f7
jeremyfelt <jeremy.felt@gmail.com>
2015-07-06 10:36:26 -0700

    Individual posts should show WSU Philanthropy News as sub header

---
+++ parts/headers.php
@@ -0,0 +1,15 @@
+<?php
+
+$spine_main_header_values = spine_get_main_header();
+
+if ( spine_get_option( 'main_header_show' ) == 'true' ) : ?>
+	<header class="main-header">
+		<div class="header-group hgroup guttered padded-bottom short">
+			<?php if ( is_singular( 'post' ) ) : ?>
+				<sub class="sub-header">WSU Philanthropy News</sub>
+			<?php else: ?>
+				<sub class="sub-header"><span class="sub-header-default"><?php echo strip_tags( $spine_main_header_values['sub_header_default'], '<a>' ); ?></span></sub>
+			<?php endif; ?>
+		</div>
+	</header>
+<?php endif;
